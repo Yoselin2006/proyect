d4f46f82f335b08c2e8d489acfcff6e27238ff09
Yoselin2006 <esmeralda.ortiz1779@alumnos.udg.mx>
2024-09-03 18:56:18 -0600

    p5 signin

--- hope.py
+++ hope.py
@@ -4,6 +4,14 @@ hope = Flask(__name__)
 
 @hope.route('/')
 def home(): 
-    return render_template('index.html')
+    return render_template('home.html')
+@hope.route('/signup')
+def signup(): 
+    return render_template('signup.html')
+@hope.route('/signin')
+def signin(): 
+    return render_template('signin.html')
+
+
 if __name__ == "_main_":
-    hope.run(debug=True,port=3300)
+    hope.run(debug=True,port=3300) 
