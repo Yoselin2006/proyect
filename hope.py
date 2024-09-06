298413f462f3e2582a963f9cef4b66dc531d9fed
Yoselin2006 <esmeralda.ortiz1779@alumnos.udg.mx>
2024-09-05 18:30:06 -0600

    p7 hash

--- hope.py
+++ hope.py
@@ -1,9 +1,19 @@
-from flask import Flask,render_template,url_for
-
+from flask import Flask,render_template,url_for,request,redirect
+from flask_mysqldb import MySQL 
+from werkzeug.security import generate_password_hash
+from datetime import datetime
 hope = Flask(__name__)
+db   = MySQL(hope)
 
 @hope.route('/')
 def home(): 
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        clave = request.form['clave']
+        clavecifrada = generate_password_hash(clave)
+        fechareg = datetime.now()
+
     return render_template('home.html')
 @hope.route('/signup')
 def signup(): 
